f487948ce55e78cb49b1b35fdb3f29521c645940
DAM2 <wowmewet1@gmail.com>
2024-11-27 16:22:48 +0100

    se ha realizado la tarea 5.1

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/main-page.component';
-
-
+import { AddPersonajeComponent } from './components/add-personaje/add-personaje.component';
+import { ListaComponent } from './components/lista/lista.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, AddPersonajeComponent, ListaComponent],
   imports: [
     CommonModule
-  ],
+],
   exports: [MainPageComponent]
 })
 export class DbzModule { }
