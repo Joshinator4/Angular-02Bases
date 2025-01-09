6c589a770e79d74d1b256e901d5be296f700a6bf
DAM2 <wowmewet1@gmail.com>
2025-01-09 17:31:24 +0100

    Subiendo cambio en docs

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MainPageComponent } from './pages/main-page.component';
 import { AddPersonajeComponent } from './components/add-personaje/add-personaje.component';
 import { ListaComponent } from './components/lista/lista.component';
